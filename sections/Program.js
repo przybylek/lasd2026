7524b2190827e33e1d5264120c9151b2491bfec9
Adam Przybylek <adam.przybylek@gmail.com>
2026-01-25 11:21:30 +0100

    Improved style

--- sections/Program.js
+++ sections/Program.js
@@ -1,76 +1,83 @@
-import Container from 'react-bootstrap/Container'
-import Table from 'react-bootstrap/Table'
-import Image from 'react-bootstrap/Image'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import React from 'react'
-import Clock from 'react-live-clock'
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
 
-/*
 const Program = () => (
-    <Container className="bg-white section p-4 my-5 rounded">
-      <a className='target' id='program'/>
-      <h2 className='display-4 anchor mb-3'>Program</h2>
-      <p>
-          TBA...
-      </p>
+    <Container className="bg-white section p-4 my-5 rounded shadow-sm">
+        <a className='target' id='program' />
+        <h2 className='display-4 anchor mb-4'>Program</h2>
+
+        <Table responsive hover bordered className="mt-4 align-middle">
+            <tbody>
+                {/* --- Dzień 1 --- */}
+                <tr className='table-primary border-primary'>
+                    {/* Tutaj dodano text-nowrap, aby data się nie łamała */}
+                    <th className="text-nowrap text-center py-3" style={{ width: '15%' }}>Tue., March 24</th>
+                    <th className="py-3">Room: Orion; chair: <em>Aleksander Jarzebowicz</em></th>
+                </tr>
+                
+                <tr>
+                    <td className='text-nowrap fw-bold text-center text-secondary'>16:00 - 16:05</td>
+                    <td>
+                        <div className="fw-bold">Track opening</div>
+                        <div className="text-muted small fst-italic">Adam Przybylek</div>
+                    </td>
+                </tr>
+                
+                <tr>
+                    <td className='text-nowrap fw-bold text-center text-secondary'>16:05 - 16:25</td>
+                    <td>
+                        <div className="fw-bold">Onboarding newcomers in Large-scale Agile projects</div>
+                        <div className="text-muted small fst-italic">Pavithra Herath, Muhammad Ovais Ahmad and Tomas Gustavsson</div>
+                    </td>
+                </tr>
+                
+                <tr>
+                    <td className='text-nowrap fw-bold text-center text-secondary'>16:25 - 16:45</td>
+                    <td>
+                        <div className="fw-bold">Combining Textual Embeddings and Graph Neural Networks for Agile Software Effort Estimation</div>
+                        <div className="text-muted small fst-italic">Carlos Bitencourt, Marcelo Augusto Santos Turine, Vanessa Araujo Borges and Bruno Nogueira</div>
+                    </td>
+                </tr>
+                
+                <tr>
+                    <td className='text-nowrap fw-bold text-center text-secondary'>16:45 - 17:05</td>
+                    <td>
+                        <div className="fw-bold">Supporting Agility at Scale Through an Organization-wide Software Engineering Community of Practice: Insights from a Large Software Development Organization</div>
+                        <div className="text-muted small fst-italic">Franziska Tobisch, Klaus Häuptle and Florian Matthes</div>
+                    </td>
+                </tr>
+                
+                <tr>
+                    <td className='text-nowrap fw-bold text-center text-secondary'>17:05 - 17:25</td>
+                    <td>
+                        <div className="fw-bold">A Framework of Critical Success Factors for Agile Software Development</div>
+                        <div className="text-muted small fst-italic">Ridewaan Hanslo and Maureen Tanner</div>
+                    </td>
+                </tr>
+
+                {/* --- Separator --- */}
+                <tr style={{ height: '20px', border: 'none' }}>
+                    <td colSpan={2} className="bg-light border-0"></td>
+                </tr>
+
+                {/* --- Dzień 2 --- */}
+                <tr className='table-primary border-primary'>
+                    <th className="text-nowrap text-center py-3">Wed., March 25</th>
+                    <th className="py-3">Room: Lobby; chair: <em>Ya-Shu Chen</em></th>
+                </tr>
+                
+                <tr>
+                    <td className='text-nowrap fw-bold text-center text-secondary'>15:00 - 17:00</td>
+                    <td>
+                        <div className="fw-bold">Leveraging Automated Risk Identification in Scrum with a k-NN Recommender System</div>
+                        <div className="text-muted small fst-italic">Ademar França De Sousa Neto, Danyllo Albuquerque, Emanuel Filho, Felipe Ramos, Luiz Antônio, Salatiel Dantas Silva, Ramon Santos, Mirko Perkusich, Hyggo Almeida and Angelo Perkusich</div>
+                    </td>
+                </tr>
+
+            </tbody>
+        </Table>
     </Container>
 )
-*/
 
-
-const Program = () => (
-    <Container className="bg-white section p-4 my-5 rounded">
-    <a className='target' id='program'/>
-    <h2 className='display-4 anchor mb-3'>Program</h2>
-
-
-
-    <Table bordered className="mt-5">
-      <tbody>	
-
-        <tr className='text-center table-primary'><th>Tue., March 24</th><th>Room: Orion; chair: <em>Aleksander Jarzebowicz</em></th></tr>
-        <tr><td className='text-nowrap'>16:00 - 16:05</td><td>Track opening (<em>Adam Przybylek</em>)</td></tr>
-        <tr><td className='text-nowrap'>16:05 - 16:25</td><td>Onboarding newcomers in Large-scale Agile projects (<em>Pavithra Herath, Muhammad Ovais Ahmad and Tomas Gustavsson</em>)</td></tr>
-        <tr><td className='text-nowrap'>16:25 - 16:45</td><td>Combining Textual Embeddings and Graph Neural Networks for Agile Software Effort Estimation (<em>Carlos Bitencourt, Marcelo Augusto Santos Turine, Vanessa Araujo Borges and Bruno Nogueira</em>)</td></tr>
-        <tr><td className='text-nowrap'>16:45 - 17:05</td><td>Supporting Agility at Scale Through an Organization-wide Software Engineering Community of Practice: Insights from a Large Software Development Organization (<em>Franziska Tobisch, Klaus Häuptle and Florian Matthes</em>)</td></tr>
-        <tr><td className='text-nowrap'>17:05 - 17:25</td><td>A Framework of Critical Success Factors for Agile Software Development (<em>Ridewaan Hanslo and Maureen Tanner</em>)</td></tr>
-
-        <tr><th className='text-center table-secondary' colSpan={2}></th></tr>
-        <tr className='text-center table-primary'><th>Wed., March 25</th><th>Room: Lobby; chair: <em>Ya-Shu Chen</em></th></tr>
-        <tr><td className='text-nowrap'>15:00 - 17:00</td><td>Leveraging Automated Risk Identification in Scrum with a k-NN Recommender System (<em>Ademar França De Sousa Neto, Danyllo Albuquerque, Emanuel Filho, Felipe Ramos, Luiz Antônio, Salatiel Dantas Silva, Ramon Santos, Mirko Perkusich, Hyggo Almeida and Angelo Perkusich</em>)</td></tr>
-
-      </tbody>
-    </Table>
-  </Container>
-)
-
-
-export default Program
-
-
-//    <Row className="justify-content-center my-5">
-//      <Col xs lg="1"> 
-//      </Col>
-//
-//      <Col xs lg="2"> 
-//        <Image fluid src='/LNBIP_LASD22.jpg' alt='LNBIP_LASD22'/>
-//      </Col>
-//      <Col>
-//  <h4>Lean and Agile Software Development</h4>
-//  <h5>6th International Conference, LASD 2022, Virtual Event, January 22, 2022, Proceedings</h5>
-//  <h4><a href="https://link.springer.com/book/10.1007/978-3-030-94238-0">LNBIP, volume 438</a></h4>          
-//      <p className='mt-4'>Time zone of the conference: GMT+1 (Central European Standard Time)</p>
-//      <p>Current Time:
-//         <Clock 
-//           format={'HH:mm:ss'}
-//           ticking={true}
-//           className="mx-2" />
-//       </p>
-//      </Col>
-//     <Col xs lg="1"> 
-//      </Col>
-//    </Row> 
-
-
-  
+export default Program;
